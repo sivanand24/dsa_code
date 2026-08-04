b99b4a526c9672cc6553b1bbb81a2dd92e9464ae
Sivanand Mishra <sivanandmishra24@gmail.com>
2026-08-04 22:07:15 +0530

    array problems with solution

--- src/ArrayBasedQuestion.java
+++ src/ArrayBasedQuestion.java
@@ -1,2 +1,29 @@
+import java.util.Arrays;
+
 public class ArrayBasedQuestion {
+    // remove duplicate in sorted array
+    public int removeDuplicates(int[] nums) {
+        if(nums.length == 0){
+            return 0;
+        }
+        int officer =0;
+        for(int i=1; i< nums.length; i++){
+            if(nums[i] != nums[officer]){
+                officer++;
+                nums[officer] = nums[i];
+            }
+        }
+        return officer +1;
+    }
+
+    //find square of elements in array
+    public int[] sortedSquares(int[] nums) {
+
+        int[] squaredNum = new int[nums.length];
+        for(int i=0;i<nums.length;i++){
+            squaredNum[i] = nums[i]*nums[i];
+        }
+        Arrays.sort(squaredNum);
+        return squaredNum;
+    }
 }

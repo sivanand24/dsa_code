dd643f8cbccb23b1cd7dc345e6a28c968da0f5a9
Sivanand Mishra <sivanandmishra24@gmail.com>
2026-08-14 23:18:06 +0530

    another and efficient approach for square elements of sorted array

--- src/ArrayBasedQuestion.java
+++ src/ArrayBasedQuestion.java
@@ -1,4 +1,4 @@
-import java.util.Arrays;
+import java.util.*;
 
 public class ArrayBasedQuestion {
     // remove duplicate in sorted array
@@ -26,22 +26,66 @@ public class ArrayBasedQuestion {
         Arrays.sort(squaredNum);
         return squaredNum;
     }
+// find the square of arr element in optimized way
+    public static int[] squareOfSum(int[] nums){
+        int n = nums.length;;
+        List<Integer> neg = new ArrayList<>();
+        List<Integer> pos = new ArrayList<>();
 
-    public int[] sortedSquaresFor(int[] nums) {
-
-        int[] squaredNum = new int[nums.length];
-        for (int i = 0; i < nums.length; i++) {
-            squaredNum[i] = nums[i] * nums[i];
+        for(int num : nums){
+            if(num <0){
+                neg.add(num);
+            } else {
+                pos.add(num);
+            }
         }
-        Arrays.sort(squaredNum);
-        return squaredNum;
-    }
-    public void sortedSquaresFo(int[] nums) {
+        //for no negative element
+        if(neg.size()==0){
+            for(int i =0; i<pos.size(); i++){
+                pos.set(i,pos.get(i)*pos.get(i));
+            }
+            return pos.stream().mapToInt(Integer::intValue).toArray();
+        }
+        //for no positive number
+        if(pos.size()==0){
+            for(int i =0; i<neg.size(); i++){
+                neg.set(i,neg.get(i)*neg.get(i));
+            }
+            Collections.reverse(neg);
+            return neg.stream().mapToInt(Integer::intValue).toArray();
+        }
+        // if the array contains both positive nad negative element
+        int n1 = neg.size();
+        int n2 = pos.size();
+        int i =0, j=0, id = 0;
+        int[] res = new int[n1 + n2];
 
-        int[] squaredNum = new int[nums.length];
-        for (int i = 0; i < nums.length; i++) {
-            squaredNum[i] = nums[i] * nums[i];
+        for( i =0; i<n1; i++) {
+            neg.set(i, neg.get(i) * neg.get(i));
+        }
+        Collections.reverse(neg);
+
+        for( i =0; i<n2; i++) {
+            pos.set(i, pos.get(i) * pos.get(i));
+        }
+        while(i < n1&& j<n2){
+            if(neg.get(i)<= pos.get(i)){
+                res[id++] = neg.get(i++);
+            }
+            else {
+                res[id++] = pos.get(j++);
+
+            }
+        }
+        while(i <n1){
+            res[id++] = neg.get(i++);
+        }
+        while(j<n2){
+            res[id++] = pos.get(j++);
         }
 
+
+return res;
     }
+
 }

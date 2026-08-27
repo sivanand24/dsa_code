1dddb6815ce10ef1158d881f9063cc97e8da2467
Sivanand Mishra <sivanandmishra24@gmail.com>
2026-08-27 14:42:24 +0530

    max consecutive ones simple problem

--- src/ArrayBasedQuestion.java
+++ src/ArrayBasedQuestion.java
@@ -330,5 +330,19 @@ return res;
         }
         return res;
     }
+    //max consecutive ones simple problem
+    public int findMaxConsecutiveOnes(int[] nums) {
+        int count = 0;
+        int max = 0;
+        for(int high = 0; high < nums.length;high++){
+            if(nums[high]==1){
+                count++;
+            } else {
+                count = 0;
+            }
+            max = Math.max(max , count);
+        }
+        return max;
+    }
 
 }

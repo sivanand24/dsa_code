0a3badb0934479769faab30f0a15280a0d0bf840
Sivanand Mishra <sivanandmishra24@gmail.com>
2026-08-27 14:16:32 +0530

    max consecutive ones ||| problem similar to repeated character replacement sum

--- src/ArrayBasedQuestion.java
+++ src/ArrayBasedQuestion.java
@@ -310,5 +310,25 @@ return res;
         }
         return res;
     }
+    //max consecutive ones |||
+    public int longestOnes(int[] nums, int k) {
+        int low = 0;
+        int zeros = 0;
+        int res = Integer.MIN_VALUE;
+        for(int high =0; high<nums.length;high++){
+            if(nums[high]==0){
+                zeros++;
+            }
+            while(zeros>k){
+                if(nums[low]==0){
+                    zeros--;
+                }
+                low++;
+            }
+            int len = high - low +1;
+            res = Math.max(res , len);
+        }
+        return res;
+    }
 
 }

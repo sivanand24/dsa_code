379a38f71efd776e24a90488389f9edbf3429962
Sivanand Mishra <sivanandmishra24@gmail.com>
2026-08-28 22:54:22 +0530

    problems: moves zeros, remove element;

--- src/ArrayBasedQuestion.java
+++ src/ArrayBasedQuestion.java
@@ -344,5 +344,29 @@ return res;
         }
         return max;
     }
+    //moves zeros
+    public void moveZeroes(int[] nums) {
+        int left = 0;
+        for(int right = 0;right <nums.length;right++){
+            if(nums[right] != 0){
+                int temp = nums[left];
+                nums[left] = nums[right];
+                nums[right] = temp;
+                left++;
+            }
+
+        }
+    }
+    //remove elements
+    public int removeElement(int[] nums, int val) {
+        int left = 0;
+        for(int right = 0;right <nums.length;right++){
+            if(nums[right]!=val){
+                nums[left]= nums[right];
+                left++;
+            }
+        }
+        return left;
+    }
 
 }

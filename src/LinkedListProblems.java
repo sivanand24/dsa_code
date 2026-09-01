661f712b0d65d5b9b3b513d57990fd081477034e
Sivanand Mishra <sivanandmishra24@gmail.com>
2026-09-01 21:40:01 +0530

    detect the start point of cycle problem

--- src/LinkedListProblems.java
+++ src/LinkedListProblems.java
@@ -15,6 +15,27 @@ public class LinkedListProblems {
         }
         return false;
     }
+    //detect the start point of cycle problem
+    public ListNode detectCycle(ListNode head) {
+        if(head == null){
+            return null;
+        }
+        ListNode slow = head;
+        ListNode fast = head;
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+            if(slow == fast){
+                slow = head;
+                while(slow != fast){
+                    slow = slow.next;
+                    fast = fast.next;
+                }
+                return slow;
+            }
+        }
+        return null;
+    }
 
     private class ListNode {
         public ListNode next;

9a372d237cb3d711843ce7dc4944ead7f59cc377
Sivanand Mishra <sivanandmishra24@gmail.com>
2026-09-01 21:56:44 +0530

    find middle of the linkedList

--- src/LinkedListProblems.java
+++ src/LinkedListProblems.java
@@ -36,6 +36,19 @@ public class LinkedListProblems {
         }
         return null;
     }
+    // find middle of the linkedList
+    public ListNode middleNode(ListNode head) {
+        if(head == null){
+            return null;
+        }
+        ListNode fast = head;
+        ListNode slow = head;
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
 
     private class ListNode {
         public ListNode next;

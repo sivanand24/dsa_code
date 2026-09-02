4cc4b83c649d759036632afb025edbc37cbbc361
Sivanand Mishra <sivanandmishra24@gmail.com>
2026-09-02 23:36:33 +0530

    happy number problem

--- src/LinkedListProblems.java
+++ src/LinkedListProblems.java
@@ -67,6 +67,29 @@ public class LinkedListProblems {
             }
         }
     }
+    //happy number problem
+    public int fun(int n){
+        int sum = 0;
+        while(n >0){
+            int digit = n%10;
+            n = n/10;
+            sum = sum + digit * digit;
+        }
+        return sum;
+    }
+    public boolean isHappy(int n) {
+        int slow = n;
+        int fast = n;
+        while(fast != 1){
+            slow = fun(slow);
+            fast = fun(fast);
+            fast = fun(fast);
+            if( slow == fast && slow != 1){
+                return false;
+            }
+        }
+        return true;
+    }
 
     private class ListNode {
         public ListNode next;

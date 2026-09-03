e31c26452d3bc549f2da55cdd4a86e5eb82f8689
Sivanand Mishra <sivanandmishra24@gmail.com>
2026-09-03 19:56:27 +0530

    minimum subarray using kadane algo

--- src/KadaneAlgo.java
+++ src/KadaneAlgo.java
@@ -11,4 +11,17 @@ public class KadaneAlgo {
         }
         return ans;
     }
+    //minimum subarray using kadane algo
+    public int minSubarraySum(int[] arr) {
+        int bestending = arr[0];
+        int ans = arr[0];
+        for(int i =1;i<arr.length;i++){
+            int v1 = bestending + arr[i];
+            int v2 = arr[i];
+            bestending = Math.min(v1,v2);
+            ans = Math.min(ans,bestending);
+        }
+
+        return ans;
+    }
 }

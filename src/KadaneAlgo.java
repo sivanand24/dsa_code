61defec3949f156f2f4ad6f2eba7f36b3c493cde
Sivanand Mishra <sivanandmishra24@gmail.com>
2026-09-04 16:33:01 +0530

    maximum product subarray

--- src/KadaneAlgo.java
+++ src/KadaneAlgo.java
@@ -24,4 +24,19 @@ public class KadaneAlgo {
 
         return ans;
     }
+    //maximum product subarray
+    public int maxProduct(int[] nums) {
+        int res = nums[0];
+        int minEnding = nums[0];
+        int maxEnding = nums[0];
+        for(int i = 1; i < nums.length; i++){
+            int v1 = nums[i];
+            int v2 = minEnding * nums[i];
+            int v3 = maxEnding * nums[i];
+            minEnding = Math.min(v1 , Math.min(v2,v3)); //to handle negative multiplication
+            maxEnding = Math.max(v1 , Math.max(v2,v3)); //to handle positive multiplication
+            res = Math.max(res,Math.max(minEnding,maxEnding));
+        }
+        return res;
+    }
 }

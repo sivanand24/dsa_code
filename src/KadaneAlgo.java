78eae7ecf7fc84ff0c3564be6374d6b67fea3594
Sivanand Mishra <sivanandmishra24@gmail.com>
2026-09-04 23:58:12 +0530

    maximum subarray sum with one deletion

--- src/KadaneAlgo.java
+++ src/KadaneAlgo.java
@@ -39,4 +39,25 @@ public class KadaneAlgo {
         }
         return res;
     }
+    //maximum subarray sum with one deletion
+    public int maximumSum(int[] arr) {
+        int nodelete = arr[0];
+        int onedelete = Integer.MIN_VALUE;
+        int res = arr[0];
+        for(int i = 1; i < arr.length; i++){
+            int prevnodelete = nodelete;
+            int prevonedelete = onedelete;
+            nodelete = Math.max(nodelete + arr[i],arr[i]);
+            int v2 = 0;
+            if(prevonedelete == Integer.MIN_VALUE){
+                v2 = arr[i];
+            }
+            else {
+                v2 = prevonedelete + arr[i];
+            }
+            onedelete = Math.max(v2 , prevnodelete);
+            res = Math.max(res,Math.max(nodelete, onedelete));
+        }
+        return res;
+    }
 }

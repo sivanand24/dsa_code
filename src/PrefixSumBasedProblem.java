ff94af3bae770394455b9e14a80facb22061ff89
Sivanand Mishra <sivanandmishra24@gmail.com>
2026-09-08 11:47:21 +0530

    subarray sum equals to k

--- src/PrefixSumBasedProblem.java
+++ src/PrefixSumBasedProblem.java
@@ -36,5 +36,23 @@ public class PrefixSumBasedProblem {
             map.put(remainder , map.getOrDefault(remainder, 0) + 1);
         }
         return count;
+    }//subarray sum Equals to k
+
+    public int subarraySum(int[] nums, int k) {
+
+        int count = 0;
+        int sum = 0;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        map.put(0,1);
+        for(int num : nums){
+            sum = sum + num;
+
+            if(map.containsKey(sum - k)){
+                count = count + map.get(sum - k);
+            }
+            map.put(sum , map.getOrDefault(sum , 0) + 1);
+        }
+        return count;
+
     }
 }

4223da59e1c1686f36ce8acb53780fd13276856b
Sivanand Mishra <sivanandmishra24@gmail.com>
2026-09-10 22:25:42 +0530

    Leet Code 167 problem for two sum

--- src/ArrayBasedQuestion.java
+++ src/ArrayBasedQuestion.java
@@ -425,5 +425,23 @@ return res;
         }
         return reslen == Integer.MAX_VALUE? "" :s.substring(reslow, reshigh +1);
     }
-
+    /* Leet Code 167 problem for two sum */
+    public int[] twoSum2 (int[] numbers, int target) {
+        int left = 0;
+        int right = numbers.length -1;
+        int sum = 0;
+        while (left < right){
+            sum = numbers[left] + numbers[right];
+            if(sum == target){
+                return new int[] {left +1, right+1};
+            }
+            else if(sum < target){
+                left++;
+            }
+            else {
+                right--;
+            }
+        }
+        return numbers;
+    }
 }

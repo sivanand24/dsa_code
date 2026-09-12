e83d20c3dab504ad3b29bd070adf88338920c622
Sivanand Mishra <sivanandmishra24@gmail.com>
2026-09-12 23:36:03 +0530

    find the squareRoot of the given number

--- src/MathBasedSums.java
+++ src/MathBasedSums.java
@@ -21,4 +21,15 @@ public class MathBasedSums {
         }
         return false;
     }
+    //find squareRoot of given number
+    public int mySqrt(int x) {
+        if (x < 2) return x;
+
+        for (int i = 1; i <= x / 2; i++) {
+            if (i <= x / i && (i + 1) > x / (i + 1)) {
+                return i;
+            }
+        }
+        return 1;
+    }
 }

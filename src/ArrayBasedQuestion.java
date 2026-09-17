51903b34505bfa20de45eb52cf6f46f4714276c4
Sivanand Mishra <sivanandmishra24@gmail.com>
2026-09-17 23:21:27 +0530

    reverse vowels if found

--- src/ArrayBasedQuestion.java
+++ src/ArrayBasedQuestion.java
@@ -461,4 +461,33 @@ return res;
             k--;
         }
     }
+    //reverse vowels if found
+    public String reverseVowels(String s) {
+        boolean[] isVowel = new boolean[128];
+        for(char c : "AEIOUaeiou".toCharArray() ){
+            isVowel[c] = true;
+        }
+        char[] chars = s.toCharArray();
+        int left = 0;
+        int right = chars.length - 1;
+        while(left < right){
+            while(left < right && !isVowel[chars[left]]){
+                left++;
+            }
+            while(left < right && !isVowel[chars[right]]){
+                right--;
+            }
+            if(left < right){
+                char temp = chars[left];
+                chars[left] = chars[right];
+                chars[right] = temp;
+                left++;
+                right--;
+            }
+        }
+
+
+
+        return new String(chars);
+    }
 }

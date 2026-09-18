1f2cd97752f07f4146dc8de8c262648ddb0873bc
Sivanand Mishra <sivanandmishra24@gmail.com>
2026-09-18 10:22:04 +0530

    linkedList palindrome problem using fast & slow pointer with two pointer approach

--- src/LinkedListProblems.java
+++ src/LinkedListProblems.java
@@ -90,8 +90,52 @@ public class LinkedListProblems {
         }
         return true;
     }
+    // linkedList palindrome
+    public boolean isPalindrome(ListNode head) {
+        if(head == null && head.next == null){
+            return true;
+        }
+        ListNode slow = head;
+        ListNode fast = head;
+        while(fast.next != null && fast.next.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        ListNode prev = null;
+        ListNode curr = slow.next;
+        while(curr != null){
+            ListNode nxt = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = nxt;
+        }
+        ListNode left = head;
+        ListNode right = prev;
+        while(right != null){
+            if(right.val != left.val){
+                return false;
+            }
+            left = left.next;
+            right = right.next;
+        }
+        return true;
+    }
 
     private class ListNode {
-        public ListNode next;
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
     }
 }

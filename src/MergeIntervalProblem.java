562c39885b8428172e666247ab8057ee3b3e5315
Sivanand Mishra <sivanandmishra24@gmail.com>
2026-09-20 22:24:01 +0530

    Interval List Intersection

--- src/MergeIntervalProblem.java
+++ src/MergeIntervalProblem.java
@@ -58,7 +58,37 @@ public class MergeIntervalProblem {
         }
         res.add(new int[]{start1,end1});
         return res.toArray(new int[res.size()][]);
-
-
+    }
+    //Interval List Intersection
+    public int[][] intervalIntersection(int[][] firstList, int[][] secondList) {
+        ArrayList<int[]> res = new ArrayList<>();
+        int i =0,j=0;
+        while( i< firstList.length && j < secondList.length){
+            int start1 = firstList[i][0];
+            int end1   = firstList[i][1];
+            int start2 = secondList[j][0];
+            int end2   = secondList[j][1];
+            if(start1 <= start2){
+                if(end1 >= start2){
+                    int s = Math.max(start1, start2);
+                    int e = Math.min(end1, end2);
+                    res.add(new int[]{s,e});
+                }
+            }
+            else {
+                if(end2 >=start1){
+                    int s = Math.max(start1, start2);
+                    int e = Math.min(end1, end2);
+                    res.add(new int[]{s,e});
+                }
+            }
+            if(end1<=end2){
+                i++;
+            }
+            else {
+                j++;
+            }
+        }
+        return res.toArray(new int[0][]);
     }
 }
